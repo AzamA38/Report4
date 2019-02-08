199004c67036ef525c9fc7130e28cf65f9fe6c09
e185731 <e185731@ie.u-ryukyu.ac.jp>
2019-02-08 23:33:51 +0900

    step3

--- src/jp/ac/uryukyu/ie/e185731/main.java
+++ src/jp/ac/uryukyu/ie/e185731/main.java
@@ -2,12 +2,7 @@ package jp.ac.uryukyu.ie.e185731;
 
 public class main {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println(e + "が発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
