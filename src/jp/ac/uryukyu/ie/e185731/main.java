10e1a7fe0bf52918ffd509a0d6a849e2bc6a7224
e185731 <e185731@ie.u-ryukyu.ac.jp>
2019-02-08 23:28:35 +0900

    step２

--- src/jp/ac/uryukyu/ie/e185731/main.java
+++ src/jp/ac/uryukyu/ie/e185731/main.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e185731;
 
 public class main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e){
+            System.out.println(e + "が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
